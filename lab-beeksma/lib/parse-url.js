3ab5cb90a6d42656fdc6fa4950bcaa646e28b445
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-08-23 10:56:02 -0500

    set up url and query parses

---
+++ lab-beeksma/lib/parse-url.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const url = require('url');
+const querystring = require('querystring');
+
+module.exports = function(req){
+  req.url = url.parse(req.url);
+  req.url.query = querystring.parse(req.url.query);
+
+  
+};
---
+++ lab-beeksma/lib/router.js
@@ -0,0 +1,3 @@
+'use strict';
+
+const parseURL = require('./parse-url');
