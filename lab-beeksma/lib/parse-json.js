341544139c9d71d4f986b46616df5bea723f025f
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-08-23 11:08:40 -0500

    parse JSON

---
+++ lab-beeksma/lib/parse-json.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = function(req){
+  if (req.method !== 'POST' && req.method !== 'PUT'){
+    return Promise.resolve(req);
+  }
+  if(req.headers['content-type'] !== 'application/json'){
+    return Promise.resolve(req);
+  }
+
+  return new Promise(function(resolve,reject){
+    var body = '';
+    req.on('data', data => body += data.toString());
+
+    req.on('error', err => {
+      console.error(err);
+      reject(err);
+    });
+
+    req.on('end', () => {
+      req.body = JSON.parse(body);
+      resolve(req);
+    });
+
+  });
+};
