dc9c93a880b88e4c87fa8c20d0382833f296176c
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-08-23 11:17:12 -0500

    Router constructor and prototype route function placeholders

--- lab-beeksma/lib/router.js
+++ lab-beeksma/lib/router.js
@@ -2,3 +2,12 @@
 
 const parseURL = require('./parse-url');
 const parseJSON = require('./parse-json')
+
+const Router = module.exports = function(){
+
+};
+
+
+Router.prototype.route = function(){
+
+};
