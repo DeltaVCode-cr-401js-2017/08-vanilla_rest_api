1e8e03e977bd29dbb806a3f12ddef084e223e2b6
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-08-23 15:14:06 -0500

    the rest of what we did in class

--- lab-beeksma/server.js
+++ lab-beeksma/server.js
@@ -2,11 +2,58 @@
 
 const http = require('http');
 const PORT = process.env.PORT || 3000;
+const uuid = require('uuid');
 
 const Router = require('./lib/router');
 const router = new Router();
 
+var storage = {};
+
+router.get('/', function(req,res){
+  console.log(req.method, req.url.href);
+  console.log('body', req.body);
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
+  res.write('routed');
+  res.end();
+});
+
+router.post('/note', (req,res) =>{
+  let note = Object.assign ({
+    id: uuid.v1(),
+  }, req.body);
+  storage[note.id] = note;
+  console.log(note);
+  res.writeHead(200, {
+    'Content-Type': 'application/json'
+  });
+  res.write(JSON.stringify(note));
+  res.end();
+});
+
+router.get('/note', (req, res) =>{
+  if(!req.url.query.id){
+    //bad request
+    res.end();
+  }
+  var note = storage[req.url.query.id];
+  if (note) {
+    res.writeHead(200, {
+      'Content-Type': 'application/json'
+    });
+    res.write(JSON.stringify(note));
+    res.end();
+  }
+  else{
+    //404
+    res.end();
+  }
+});
+
 const server = http.createServer(router.route());
+
+
 
 if(!module.parent){
   server.listen(PORT, () => {
--- lab-beeksma/test/server.test.js
+++ lab-beeksma/test/server.test.js
@@ -6,8 +6,8 @@ const { expect } = require('chai');
 
 
 
-describe('GET /', function (){
-  it('should return routed', function (done){
+describe('Routes', function (){
+  it('should return routed for /', function (done){
     request.get('/')
     .expect(200)
     .expect('routed')
@@ -16,5 +16,51 @@ describe('GET /', function (){
       expect(res.files).to.be.undefined;
     })
     .end(done);
+  });
+
+  it('should return Not Found for GET missing path', function (done){
+    request.get('/404')
+    .expect(404)
+    .expect('Not Found')
+    .expect('content-type', 'text/plain')
+    .end(done);
+  });
+
+  it('should return Not Found for POST missing path', function (done){
+    request.post('/404')
+    .expect(404)
+    .expect('Not Found')
+    .expect('content-type', 'text/plain')
+    .end(done);
+  });
+
+});
+
+describe('Simple Resource', function (){
+  var note = null;
+
+  describe('POST /note', function() {
+    it('should save body', function (done){
+      request.post('/note')
+      .send({note: 'this is a note'})
+      .expect(200)
+      .expect(res =>{
+        expect(res.body.note).to.equal('this is a note');
+        expect(res.body.id).to.not.be.empty;
+        note = res.body;
+      })
+      .end(done);
+    });
+  });
+  describe('GET /note', function (){
+    it('should return a note', function (done){
+      request.get(`/note?id=${note.id}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.note).to.equal(note.note);
+        expect(res.body.id).to.equal(note.id);
+      })
+      .end(done);
+    });
   });
 });
